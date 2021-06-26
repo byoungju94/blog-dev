2d4fb6f4bd80c21e20c25b41a028c46878743b46
byoungju94 <byoungju94@outlook.com>
2021-06-27 01:05:42 +0900

    add index.html

--- src/main/java/com/byoungju94/blog/post/Post.java
+++ src/main/java/com/byoungju94/blog/post/Post.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.domain.Persistable;
 
 import lombok.Builder;
