3e65aeb5f6a3468bd1368f31740bd2695ba8e7cf
byoungju94 <byoungju94@outlook.com>
2021-07-20 23:18:48 +0900

    add layout

--- browser/src/App.tsx
+++ browser/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Top from './layout/top/Top';
+import Bottom from './layout/bottom/Bottom';
+import Middle from './layout/middle/Middle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex flex-column h-100">
+      <Top></Top>
+      <Middle></Middle>
+      <Bottom></Bottom>
     </div>
   );
 }
--- browser/src/index.tsx
+++ browser/src/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Bottom from './layout/bottom/Bottom';
+import Middle from './layout/middle/Middle';
+import Top from './layout/top/Top';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render([<Top />, <Middle />, <Bottom />], document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
